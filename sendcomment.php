e9875251202cf2ee6a9f7fd12b4171c31f5afce1
bmad4ever <bru_15scp@hotmail.com>
2015-12-03 17:13:34 +0000

    avoid empty comments. more changes were made but will only be added after update/main/create_event reach their final version

--- sendcomment.php
+++ sendcomment.php
@@ -3,6 +3,12 @@ include("header.php");
 include("getInputSafe.php");
 
   if (isset($_SESSION['login_user']) && isset($_GET['comment']) &&  isset($_SESSION['display_event_id'])) {
+  
+  $com = cleanUserTextTags(trim($_GET['comment']));
+  
+	if($com!="")
+	{
+  
   // Current datetime
   $current_datetime = date("Y-m-d H:i:s");
   
@@ -13,9 +19,10 @@ include("getInputSafe.php");
 	  
     // Insert Comment
     $stmt = $dbh->prepare("INSERT INTO comments VALUES (null, ?, ?, ?,?)");
-    $stmt->execute(array($_SESSION['login_user'], $_SESSION['display_event_id'], $current_datetime ,cleanUserTextTags($_GET['comment']) ) );
+    $stmt->execute(array($_SESSION['login_user'], $_SESSION['display_event_id'], $current_datetime , $com) );
 	
-	echo json_encode("OK");  
+	echo json_encode("OK"); 
+	}	
   }
-else  echo json_encode("INVALID");  
+  echo json_encode("INVALID");  
 ?>
