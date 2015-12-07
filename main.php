867c2a7a6296d59f906b786bd784af61e5db44e1
theRickix <ricardo_magalhaes45@hotmail.com>
2015-12-07 17:27:39 +0000

    Fix.

--- main.php
+++ main.php
@@ -64,12 +64,12 @@ function main_display()
 	
 	<footer>
         <marquee behavior="scroll" direction="left">
-            Last Event: show last event here or something similar
+            Welcome to EventBook. Here you can share your events with your friends (or not).
         </marquee>
 
        	<br>Date  
 	<div id="date_display"><?php // echo date('l jS \of F Y h:i:s A')?></div>
-+<!-- echo date('l jS \of F Y h:i:s A'); 
+<!-- echo date('l jS \of F Y h:i:s A'); 
 		<!--print_r(getdate()); -->
 	<!--echo date('Y-m-d H:i:s'); -->
 
